32d9499f22f0f5e6c97733c274f6ba7f2908a16c
Constantine Mars <constantine.mars@gmail.com>
2015-04-07 11:03:33 +0300

    stream from array

--- src/c/mars/Main.java
+++ src/c/mars/Main.java
@@ -3,6 +3,7 @@ package c.mars;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -26,9 +27,15 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9).stream()
-                .filter(x -> x > 4 && x < 8)
-                .reduce(0, (a, b) -> a + b)); //sum
+//        System.out.println(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9).stream()
+//                .filter(x -> x > 4 && x < 8)
+//                .reduce(0, (a, b) -> a + b)); //sum
 //                .forEach(System.out::println); //print each
+
+        Integer[] a = {1, 2, 3, 4, 5, 6, 7, 8};
+        Stream<Integer> stream = Stream.of(a);
+        stream
+                .filter(x -> x>2&&x<6)
+                .forEach(System.out::println);
     }
 }
